e10e38951975949a5fdabae6b7493c48720ff299
Tony Jason Anga <tonyjason@Tonys-Air-2.lan>
2023-05-10 18:55:58 -0400

    process.env

--- index.js
+++ index.js
@@ -154,6 +154,6 @@ app.get("/products/viewWinner/:tokenId", async (req,res) =>{
   return res.json(winToken);
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
